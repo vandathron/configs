2f41a239a2aa12c36292a6b3d1d73cda548213c1
vandathron <akinnagbelast@gmail.com>
2025-03-18 23:55:42 +0100

    fix: locate k8s project path

--- emacs/projectile.el
+++ emacs/projectile.el
@@ -1,7 +1,7 @@
 (use-package projectile
   :ensure t
   :init
-  (setq projectile-project-search-path '("~/dotnet/" "~/go/src" "~go/src/k8s.io" "~/kubernetes")) ;;path to most of the projects I need to work on on emacs
+  (setq projectile-project-search-path '("~/dotnet/" "~/go/src" "~/go/src/k8s.io" "~/kubernetes")) ;;path to most of the projects I need to work on on emacs
   :config
   (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
   (global-set-key (kbd "C-c p") 'projectile-command-map)
