ec133fdaad83d802e09c3827246a8ae7c59793b7
vandathron <akinnagbelast@gmail.com>
2025-03-23 19:30:32 +0100

    add fold/unfold keybinding, init.el file

---
+++ emacs/init.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t -*-
+
+;; Set package sources
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+;; Trust local .dir el files
+(setq enable-local-eval t
+      enable-local-variables :all)
+;; Enable hs-minor-mode automatically in programming modes
+(add-hook 'prog-mode-hook 'hs-minor-mode)
+
+;; Load modular configurations
+(load "~/.emacs.d/lisp/ui.el")   ;; ui management
+(load "~/.emacs.d/lisp/packages.el")   ;; Package management
+(load "~/.emacs.d/lisp/eglot.el")      ;; Eglot (LSP)
+(load "~/.emacs.d/lisp/treesitter.el") ;; Tree-sitter setup
+(load "~/.emacs.d/lisp/projectile.el") ;; Projectile (Project management)
+(load "~/.emacs.d/lisp/keybindings.el") ;; Custom keybindings
+
--- emacs/keybindings.el
+++ emacs/keybindings.el
@@ -1,4 +1,4 @@
 (global-set-key (kbd "C-x k") 'kill-this-buffer) ;; Kill current buffer
 (global-set-key (kbd "C-x C-b") 'ibuffer) ;; Better buffer list
 (global-set-key (kbd "M-x") 'execute-extended-command)
-
+(global-set-key (kbd "C-c h") 'hs-toggle-hiding) ;; Bind C-c h to toggle folding
